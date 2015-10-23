33144566285c33ff4b69d62b5f788f1673925f48
Remy Roez <remyroez@gmail.com>
2015-10-24 02:09:05 +0900

    プレイヤー（ボール）の作成
    ・操作対象のボールを作成
    ・ボールを操作するプレイヤーオブジェクトを作成
    ・カメラの位置調整

---
+++ Assets/Scripts/PlayableBallController.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayableBallController : MonoBehaviour, IPlayable
+{
+    public void Move (float x, float z)
+    {
+        Rigidbody rigidbody = GetComponent<Rigidbody>();
+        if (rigidbody)
+        {
+            rigidbody.AddForce(x * 10, 0, z * 10);
+        }
+    }
+}
